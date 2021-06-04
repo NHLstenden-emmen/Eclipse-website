ab51335050c13f953610adf2ba2138554052c6e8
Kevin Smulders <smulders.kevin.1999@gmail.com>
2021-06-04 14:50:22 +0200

    remove old stuf

--- example.env.php
+++
@@ -1,8 +0,0 @@
-<?php
-	return [
-		'DB_HOST' => 'fill',
-		'DB_USERNAME' => 'fill',
-		'DB_PASSWORD' => 'fill',
-		'DB_TABLE' => 'fill'
-	]
-?>
--- inc/navbar.php
+++
@@ -1,6 +0,0 @@
-<nav>
-	<a href="home">
-		<img src="images/logo/bigLogo.png" alt="Eclipse Logo">
-	</a>
-	<p><a href="login">login</a></p>
-</nav>
