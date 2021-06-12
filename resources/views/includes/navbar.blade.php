c4e8b14d198b47b1c459f3fb44243c5c6e72b737
Kevinsmul <smulders.kevin.1999@gmail.com>
2021-06-12 16:15:22 +0200

    responsive navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,3 +1,12 @@
+<div class="topnav" id="myTopnav">
+	<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+		<i class="fa fa-bars"></i>
+	</a>
+	<a href="/" class="{{ (request()->is('/*')) ? 'active' : '' }}">Home</a>
+	<a href="mirrorSpecs" class="{{ (request()->is('mirrorSpecs')) ? 'active' : '' }}">mirrorSpecs</a>
+	<a href="widgets" class="{{ (request()->is('widgets')) ? 'active' : '' }}">widgets</a>
+	<a href="setup" class="{{ (request()->is('setup')) ? 'active' : '' }}">setup</a>
+</div>
 <nav>
 	<a href="/">
 		<img src="img/logo/bigLogo.png" alt="Eclipse Logo">
@@ -8,3 +17,13 @@
 		<p><a class="nav-link" href="registration">Sign up</a></p>
 	@endauth
 </nav>
+<script>
+  function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  }
+</script>
